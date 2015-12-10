af4d6f37c6c61c54890a1ec5bf92a249d0b53dd1
kiang <kiange@gmail.com>
2015-12-11 02:16:57 +0800

    remove infobox as it caused position change on zooming

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $.ajaxSetup({async: false});
 
 var map, points, info, bounds, data, codes = {}, meta, currentDate, loadedData = {};
 var dateBegin = new Date('2015-11-16'), dateEnd, firstCsv = true, selectedPoint = false;
-var standards = {}, markers = {}, overlays = [], markerClicked = false;
+var standards = {}, markers = {}, markerClicked = false;
 
 $.getJSON('http://ks-opendata-community.github.io/chimney/data/工廠清單.json', {}, function (p) {
     points = p;
@@ -73,15 +73,6 @@ function initialize() {
         markers[p['管制編號']] = marker;
         bounds.extend(geoPoint);
 
-        var ibLabel = new InfoBox({
-            content: p['工廠'],
-            position: geoPoint,
-            boxClass: 'labelPoint',
-            closeBoxURL: '',
-            pixelOffset: new google.maps.Size(-25, 0)
-        });
-        overlays.push(ibLabel);
-
         pointSelectOptions += '<option value="' + p['管制編號'] + '">' + p['工廠'] + '[' + p['管制編號'] + ']</option>';
     });
     $('#pointSelect').html(pointSelectOptions).select2();
@@ -91,23 +82,6 @@ function initialize() {
             if (markers[value]) {
                 new google.maps.event.trigger(markers[value], 'click');
             }
-        }
-    });
-
-    google.maps.event.addListener(map, 'zoom_changed', function () {
-        var zoomLevel = map.getZoom();
-        if (zoomLevel < 12) {
-            $.each(overlays, function (i, v) {
-                if (v instanceof InfoBox) {
-                    v.setMap(null);
-                }
-            });
-        } else {
-            $.each(overlays, function (i, v) {
-                if (v instanceof InfoBox) {
-                    v.setMap(map);
-                }
-            });
         }
     });
 
